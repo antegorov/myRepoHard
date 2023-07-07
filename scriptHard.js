1e7261872e3a926526ba522b0a7e85a992793ff7
antegorov <antegorov@mail.ru>
2023-07-07 19:56:07 +0500

    Оставил код из прошлого задания

--- scriptHard.js
+++ scriptHard.js
@@ -1,4 +1,16 @@
 "use strict";
+let num = 266219;
+
+let result = 1;
+num
+	.toString()
+	.split("")
+	.forEach(function (element) {
+		result *= element;
+	});
+
+console.log(result);
+console.log((result ** 3).toString().substring(0, 2));
 
 // 1)
 let lang;
