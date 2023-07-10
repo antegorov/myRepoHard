7d847c87db23dbfa783a7d2c478f8fede611d781
antegorov <antegorov@mail.ru>
2023-07-10 10:29:12 +0500

    Выполнил усложненное задание №2

--- scriptHard.js
+++ scriptHard.js
@@ -1,18 +1,21 @@
 'use strict'
 
-let variable = 5
-let newVar
+let arr = []
+arr.push(12000, 87400, 432, 5210, 220, 14, 2600)
 
-const strOption = function (variable) {
-	if (typeof variable == 'string') {
-		newVar = variable.replace(/^ +| +$/g, '')
-		if (variable.length > 30) newVar = newVar.slice(0, 30) + '...'
-	} else {
-		newVar = 'Это не строка'
-	}
-	return newVar
+console.log('\nИсходный массив:')
+for (let i = 0; i < arr.length; i++) console.log(arr[i])
+
+console.log('\nЗначения массива, которые начинаются с цифры 2 или 4:')
+for (let i = 0; i < arr.length; i++) {
+	if (String(arr[i]).search(/(2|4)/) === 0) console.log(arr[i])
 }
 
-strOption(variable)
-
-console.log(newVar)
+console.log('\nПростые число от 1 до 100:')
+for (let i = 2; i < 101; i++) {
+	let count = 0
+	for (let j = 2; j < i; j++) {
+		if (i % j == 0) count++
+	}
+	if (count < 2) console.log(i + '\tДелители этого числа: 1 и ' + i)
+}
